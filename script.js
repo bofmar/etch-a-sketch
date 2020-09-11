f05aa845c4118639d626c62763bc51b8ffb432c1
bofmar <bofmar@outlook.com>
2020-09-11 20:05:31 +0300

    Added placeholder buttons and altered the way the user draws

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const canvas = document.querySelector(".canvas-area");
+const body = document.querySelector("body");
+let canChangeColor = false;
 
 createGrid();
 
 let gridSquares = document.querySelectorAll(".canvas-item");
+gridSquares.forEach(gridSquare => gridSquare.addEventListener("mousedown", ()=>{
+    canChangeColor = true;
+})); //checks if the user has his mouse click down and enables coloring
+
+body.addEventListener("mouseup",()=>{
+    canChangeColor = false;
+}); //disable coloring if the user lifts his mouse up anywhere in the body
+
 gridSquares.forEach(gridSquare => gridSquare.addEventListener("mouseover", changeColor));
+
+//when user clicks on a cell, always color that cell
+gridSquares.forEach(gridSquare => gridSquare.addEventListener("click", clickChangeColor)); 
+
 
 function createGrid(){
     canvas.style.setProperty("--grid-size", 16);
@@ -16,5 +30,11 @@ function createGrid(){
 }
 
 function changeColor(e){
+    if(canChangeColor){
+        e.target.style.backgroundColor = "black";
+    }
+}
+
+function clickChangeColor(e){
     e.target.style.backgroundColor = "black";
 }
